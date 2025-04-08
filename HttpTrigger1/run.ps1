12066e721ee066b17ad45ba7dd8d8fcf18b55ed2
Jordan Mills <jordan@jordanmills.com>
2025-04-08 13:34:34 -0500

    3

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -6,6 +6,7 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+<#
 # Interact with query parameters or the body of the request.
 $name = $Request.Query.Name
 if (-not $name) {
@@ -17,6 +18,9 @@ $body = "This HTTP triggered function executed successfully. Pass a name in the 
 if ($name) {
     $body = "Hello, $name. This HTTP triggered function executed successfully."
 }
+#>
+
+$body = $Request.Headers
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
