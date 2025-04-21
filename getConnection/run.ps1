55b34acd62e89213899d9c20c0bd8294cc88392f
Jordan Mills <jordan@jordanmills.com>
2025-04-21 14:30:58 -0500

    trimming

--- getConnection/run.ps1
+++ getConnection/run.ps1
@@ -54,7 +54,7 @@ gatewaybrokeringtype:i:0/
 redirectwebauthn:i:1/
 enablerdsaadauth:i:0/
 gatewayaccesstoken:s:{3}
-'@.split("/").where({$_})
+'@.split("/").where({$_}) | ForEach-Object { $_.trim() }
 
 #$Request.params.hostname = $Request.params.hostname
 #$env:APPSETTING_rdgwfedauth_gwhost = $env:APPSETTING_rdgwfedauth_gwhost
